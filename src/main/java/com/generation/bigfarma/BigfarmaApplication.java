96cf91d779cec8068f3947f9a066bd5286cd37f1
jloliveira95 <jl.oliveira95@gmail.com>
2025-01-27 16:03:53 -0300

    Configuração inicial

---
+++ src/main/java/com/generation/bigfarma/BigfarmaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.bigfarma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigfarmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigfarmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/bigfarma/BigfarmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.bigfarma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigfarmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
