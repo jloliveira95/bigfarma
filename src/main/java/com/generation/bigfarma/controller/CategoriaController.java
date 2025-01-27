5f6a7cc67803f40b3cbc01fb83cedfd072ec83b7
jloliveira95 <jl.oliveira95@gmail.com>
2025-01-27 16:49:43 -0300

    crud categoria atualizado

--- src/main/java/com/generation/bigfarma/controller/CategoriaController.java
+++ src/main/java/com/generation/bigfarma/controller/CategoriaController.java
@@ -1,5 +1,44 @@
 package com.generation.bigfarma.controller;
 
-public class CategoriaController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.bigfarma.model.Categoria;
+import com.generation.bigfarma.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    public List<Categoria> listarTodos() {
+        return categoriaRepository.findAll();
+    }
+
+    @PostMapping
+    public Categoria criar(@RequestBody Categoria categoria) {
+        return categoriaRepository.save(categoria);
+    }
+
+    @PutMapping("/{id}")
+    public Categoria atualizar(@PathVariable Long id, @RequestBody Categoria categoria) {
+        categoria.setId(id);
+        return categoriaRepository.save(categoria);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        categoriaRepository.deleteById(id);
+    }
 }
